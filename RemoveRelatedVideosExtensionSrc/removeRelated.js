0b7151252d377de9573736a314b92ed5bd0c3039
amphormessenger <johnnypanos@icloud.com>
2017-09-06 21:46:46 -0700

    Made changes

--- RemoveRelatedVideosExtensionSrc/removeRelated.js
+++ RemoveRelatedVideosExtensionSrc/removeRelated.js
@@ -1,4 +1,12 @@
-if (document.title.indexOf("YouTube") != -1) {
-    main = document.getElementById('main');
-    main.removeChild(document.getElementById('related'));
+function removeRelated() {
+    if (document.title.indexOf("YouTube") != -1) {
+        main = document.getElementById('main');
+        main.removeChild(document.getElementById('related'));
+    }
 }
+
+document.onreadystatechange = () => {
+    if (document.readyState === 'complete') {
+        removeRelated();
+    }
+};
