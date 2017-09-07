4757b02658472176dc8d66189e1bd4e74ae7890b
amphormessenger <johnnypanos@icloud.com>
2017-09-06 22:03:30 -0700

    Made changes

--- RemoveRelatedVideosExtensionSrc/removeRelated.js
+++ RemoveRelatedVideosExtensionSrc/removeRelated.js
@@ -1,12 +1,10 @@
-function removeRelated() {
-    if (document.title.indexOf("YouTube") != -1) {
-        main = document.getElementById('main');
+if (document.title.indexOf("YouTube") != -1) {
+    var main = document.getElementById('main');
+    // Some fixes for old YouTube design
+    if (main != undefined) {
         main.removeChild(document.getElementById('related'));
+    } else {
+        var main2 = document.getElementById('watch7-main');
+        main2.removeChild(document.getElementById('watch7-sidebar'));
     }
 }
-
-document.onreadystatechange = () => {
-    if (document.readyState === 'complete') {
-        removeRelated();
-    }
-};
